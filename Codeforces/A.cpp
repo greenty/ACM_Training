cb65b8cf693b23dc7da34dcba5a13a77e996d66f
greenty <779235890@qq.com>
2018-05-15 08:04:23 +0800

    Update 2018.05.15

---
+++ Codeforces/A.cpp
@@ -0,0 +1,41 @@
+#include<cstdio>
+#include<cstring>
+#include<algorithm>
+#include<iostream>
+#include<queue>
+#include<cmath>
+#include<map>
+#include<stack>
+#include<set>
+#define inc(i,l,r) for(int i=l;i<=r;i++)
+#define dec(i,l,r) for(int i=l;i>=r;i--)
+#define link(x) for(edge *j=h[x];j;j=j->next)
+#define mem(a) memset(a,0,sizeof(a))
+#define ll long long
+#define eps 1e-12
+#define succ(x) (1<<x)
+#define lowbit(x) (x&(-x))
+#define sqr(x) ((x)*(x))
+#define mid (x+y>>1)
+#define NM 200005
+#define nm 1000005
+#define pi 3.1415926535897931
+using namespace std;
+const ll inf=1e9+7;
+ll read(){
+    ll x=0,f=1;char ch=getchar();
+    while(!isdigit(ch)){if(ch=='-')f=-1;ch=getchar();}
+    while(isdigit(ch))x=x*10+ch-'0',ch=getchar();
+    return f*x;
+}
+
+
+
+
+
+
+int main(){
+    while(~scanf("%d",&n)){
+
+    }
+}
